f825e88b1d43794c51e9c80f0ff9b1a986a058f9
Dev-Maajo <gamermaajo@gmail.com>
2025-08-03 13:09:13 +0530

    commit

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models.user import User
 from models.database import db
 from functools import wraps
 
